c1c58f1c8be617cb9dcc1b4d4ab2fcd935bdd276
Doug Ashton <douglas.j.ashton@gmail.com>
2018-05-29 11:59:00 +0100

    add a simple test and travis build

---
+++ tests/test_path.py
@@ -0,0 +1,8 @@
+import pytest
+import os
+from sdg import output_path
+
+def test_out_path():
+    """Check that output_path is as expected"""
+    out_path = output_path(inid="1-2-1",  ftype='data', format='json')
+    assert out_path == os.path.join('_site', 'data', '1-2-1.json')

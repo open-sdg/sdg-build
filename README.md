27cc7a43d242ae6c1cc7785f1c26604fc1f9f730
Douglas <douglas.j.ashton@gmail.com>
2018-05-31 21:28:56 +0100

    mangothecat to ONSdigital

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SDG Build
 
-[![Build Status](https://travis-ci.org/MangoTheCat/sdg-build.svg?branch=develop)](https://travis-ci.org/MangoTheCat/sdg-build)
+[![Build Status](https://travis-ci.org/ONSdigital/sdg-build.svg?branch=develop)](https://travis-ci.org/ONSdigital/sdg-build)
 
 > The data and metadata building code for teh SDG indicators
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='sdg',
       version='0.1.0',
       description='Build SDG data and metadata into output formats',
-      url='https://github.com/mangothecat/sdg-build',
+      url='https://github.com/ONSdigital/sdg-build',
       author='Doug Ashton',
       author_email='douglas.j.ashton@gmail.com',
       license='MIT',

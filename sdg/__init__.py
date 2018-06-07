04bedaefa9916abfcdaace1b033494afd9629d45
Martin Bamford <bamfom@C02W60K0G8WL.local>
2018-06-07 15:18:17 +0100

    Remove unused reset scripts

--- sdg/__init__.py
+++ sdg/__init__.py
@@ -15,5 +15,3 @@ from . import schema
 from .check_metadata import check_all_meta
 from .check_csv import check_all_csv
 from .build import build_data
-from .reset_csvs import reset_all_csv
-from .reset_metadata import reset_all_meta

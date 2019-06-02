1f1bf84123d6956e706f2f7f2831c34145cd788c
Brock Fanning <brockfanning@gmail.com>
2019-06-01 22:42:52 -0400

    Refactor SDMX-JSON input for clarity

--- sdg/inputs/InputSdmxJsonApi.py
+++ sdg/inputs/InputSdmxJsonApi.py
@@ -35,6 +35,7 @@ class InputSdmxJsonApi(InputBase):
         self.drop_singleton_dimensions = drop_singleton_dimensions
         self.dimension_map = dimension_map
         self.indicator_map = indicator_map
+        self.series_dimensions = {}
         InputBase.__init__(self)
 
 
@@ -83,106 +84,311 @@ class InputSdmxJsonApi(InputBase):
             return None
         # Otherwise default to whatever is in the JSON.
         return dimension_value['name']
+    
+
+    def get_dimensions(self):
+        """Get the full list of "dimensions" from the SDMX-JSON.
+
+        Returns
+        -------
+        list
+            List of dimension dicts
+        """
+        return self.data['structure']['dimensions']['series']
 
 
-    def fix_indicator_name(self, name, indicator_id):
-        """Clean/trim the indicator name by trying to remove the id.
+    def get_attributes(self):
+        """Get the full list of "attributes" from SDMX-JSON.
+
+        Returns
+        -------
+        list
+            List of attribute dicts
+        """
+        return self.data['structure']['attributes']['series']
+    
+
+    def get_years(self):
+        """Get the full "time period" structure from the SDMX-JSON.
+
+        Returns
+        -------
+        dict
+            Time period structure from SDMX-JSON
+        """
+        return self.data['structure']['dimensions']['observation'][0]['values']
+
+
+    def get_all_series(self):
+        """Get the full series structure from the SDMX-JSON.
+
+        Returns
+        -------
+        dict
+            Series structure from SDMX-JSON
+        """
+        return self.data['dataSets'][0]['series']
+ 
+
+    def get_series(self, series_key):
+        """Get a particular series, by key.
 
         Parameters
         ----------
-        name : string
-            A raw indicator name
-        indicator_id : string
-            The id for this indicator.
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        dict
+            A particular series dict
         """
-        name = name.replace(indicator_id, '')
-        indicator_id = indicator_id.replace('-', '.')
-        name = name.replace(indicator_id, '')
-        return name.strip()
+        return self.get_all_series()[series_key]
+
+
+    def get_observations(self, series_key):
+        """Get the list of observations for a particular series.
+
+        Parameters
+        ----------
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        list
+            A list of observations dicts for a series
+        """
+        return self.get_series(series_key)['observations']
+
+
+    def get_dimension_values(self, value_list, dimension_list):
+        """Return a dict of dimension/values, keyed by the dimension id.
+
+        Parameters
+        ----------
+        value_list : list
+            A list with a corresponding value for each item in dimension_list
+        dimension_list : list
+            A list of dimension dicts
+
+        Returns
+        -------
+        dict
+            A custom dict structure, keyed by dimension id, containing the
+            full dimension dict and its full value dict
+        """
+        dimensions = {}
+        for dimension_index, dimension_value_index in enumerate(value_list):
+            if dimension_index is None or dimension_value_index is None:
+                continue
+            dimension_index = int(dimension_index)
+            dimension_value_index = int(dimension_value_index)
+            dimension = dimension_list[dimension_index]
+            dimension_value = dimension['values'][dimension_value_index]
+            dimensions[dimension['id']] = {
+                'dimension': dimension,
+                'value': dimension_value
+            }
+        return dimensions
+
+
+    def get_series_dimensions(self, series_key):
+        """Parse the dimensions/attributes/values for a particular series.
+
+        Parameters
+        ----------
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        dict
+            A custom dict structure, keyed by dimension id, containing the
+            full dimension/attribute dict and its full value dict.
+        """
+        # Use a previously-calculated version if available.
+        if series_key in self.series_dimensions:
+            return self.series_dimensions[series_key]
+        # Gather the dimension values for this series key.
+        value_list = series_key.split(':')
+        dimension_list = self.get_dimensions()
+        dimension_values = self.get_dimension_values(value_list, dimension_list)
+        # Also gether the attribute values for this series key.
+        value_list = self.get_series(series_key)['attributes']
+        dimension_list = self.get_attributes()
+        attribute_values = self.get_dimension_values(value_list, dimension_list)
+        # Combine and save the results.
+        dimension_values.update(attribute_values)
+        self.series_dimensions[series_key] = dimension_values
+        return dimension_values
+
+
+    def get_series_name(self, series_key):
+        """Get the name for a series.
+
+        Parameters
+        ----------
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        string
+            Name for this series
+        """
+        dimensions = self.get_series_dimensions(series_key)
+        series_name = dimensions['SERIES']['value']['name']
+        series_id = dimensions['SERIES']['value']['id']
+        if series_id in self.indicator_map:
+            indicator_id = self.indicator_map[series_id]
+            dashes = indicator_id.replace('.', '-')
+            dots = indicator_id.replace('-', '.')
+            series_name = series_name.replace(dashes, '')
+            series_name = series_name.replace(dots, '')
+        return series_name.strip()
+
+
+    def get_series_id(self, series_key):
+        """Get the id for a series ("indicator id").
+
+        Parameters
+        ----------
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        string
+            Indicator id for this series
+        """
+        dimensions = self.get_series_dimensions(series_key)
+        series_id = dimensions['SERIES']['value']['id']
+        if series_id not in self.indicator_map:
+            return None
+        indicator_id = self.indicator_map[series_id]
+        indicator_id = indicator_id.replace('.', '-')
+        return indicator_id
+
+
+    def get_series_disaggregations(self, series_key):
+        """Get the disaggregation categories/values for a series.
+
+        Parameters
+        ----------
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        dict
+            Disaggregation values, keyed by category, for this series
+        """
+        disaggregations = {}
+        dimensions = self.get_series_dimensions(series_key)
+        for key in dimensions:
+            dimension = dimensions[key]
+            series_id = dimension['dimension']['id']
+            if series_id == 'SERIES' or series_id in self.drop_dimensions:
+                # Skip "SERIES" and anything set to be dropped.
+                continue
+            elif self.drop_singleton_dimensions and len(dimension['dimension']['values']) < 2:
+                # Ignore dimensions with only 1 variation.
+                continue
+            else:
+                # Otherwise we will use this dimension as a disaggregation.
+                dimension_name = self.get_dimension_name(dimension['dimension'])
+                value_name = self.get_dimension_value_name(dimension['dimension'], dimension['value'])
+                disaggregations[dimension_name] = value_name
+        return disaggregations
+
+
+    def get_series_data(self, series_key):
+        """Convert a set of observations into a list of rows.
+
+        Parameters
+        ----------
+        series_key : string
+            The colon-delimited series key
+
+        Returns
+        -------
+        List
+            The rows of data for this series.
+        """
+        disaggregations = self.get_series_disaggregations(series_key)
+        observations = self.get_observations(series_key)
+        rows = []
+        for observation_key in observations:
+            year = self.get_years()[int(observation_key)]['name']
+            value = observations[observation_key][0]
+            row = self.get_row(year, value, disaggregations)
+            rows.append(row)
+        return rows
+    
+
+    def create_dataframe(self, rows):
+        """Convert a list of rows into a dataframe.
+
+        Parameters
+        ----------
+        rows : List
+            A list of row dicts
+
+        Returns
+        -------
+        Dataframe
+            The dataframe of rows of data for this indicator.
+        """
+        df = pd.DataFrame(rows)
+        # Enforce position of "Year" and "Value".
+        df = self.fix_dataframe_columns(df)
+        # Remove empty columns, because they are not necessary.
+        df = df.dropna(axis='columns', how='all')
+        return df
+
+
+    def fetch_data(self):
+        """Fetch the data from the endpoint."""
+        headers = { 'Accept': 'application/json' }
+        r = requests.get(self.endpoint, headers=headers)
+        self.data = r.json()
 
 
     def execute(self):
         """Fetch the data from the SDMX endpoint. Overrides parent."""
 
-        headers = { 'Accept': 'application/json' }
-        r = requests.get(self.endpoint, headers=headers)
-        data = r.json()
+        # Fetch the response from the SDMX endpoint.
+        self.fetch_data()
 
-        series_dimensions = data['structure']['dimensions']['series']
-        series_attributes = data['structure']['attributes']['series']
-        time_periods = data['structure']['dimensions']['observation'][0]['values']
-
-        indicators = {}
+        # SDMX-JSON divides the data into series, but we want to divide
+        # the data into indicators. Indicators contain multiple series,
+        # so we need to loop through the series and build up indicators.
+        indicator_data = {}
         indicator_names = {}
 
-        for series_key in data['dataSets'][0]['series']:
-            observations = data['dataSets'][0]['series'][series_key]['observations']
-            attributes = data['dataSets'][0]['series'][series_key]['attributes']
-            for observation_key in observations:
-                observation = observations[observation_key]
-                year = time_periods[int(observation_key)]['name']
-                value = observation[0]
-                indicator_id = None
-                disaggregations = {}
-                for dimension_index, dimension_value_index in enumerate(series_key.split(':')):
-                    dimension_index = int(dimension_index)
-                    dimension_value_index = int(dimension_value_index)
-                    dimension = series_dimensions[dimension_index]
-                    dimension_value = dimension['values'][dimension_value_index]
+        # Loop through each "series" in the SDMX-JSON.
+        for series_key in self.get_all_series():
 
-                    # If this is the "SERIES" dimension, figure out the indicator ID.
-                    if dimension['id'] == 'SERIES':
-                        indicator_id = self.indicator_map[dimension_value['id']]
-                        indicator_id = indicator_id.replace('.', '-')
-                        indicator_names[indicator_id] = self.fix_indicator_name(dimension_value['name'], indicator_id)
-                    elif dimension['id'] in self.drop_dimensions:
-                        # Ignore dimensions specifically set to drop.
-                        continue
-                    elif self.drop_singleton_dimensions and len(dimension['values']) < 2:
-                        # Ignore dimensions with only 1 variation.
-                        continue
-                    else:
-                        # Otherwise we will use this dimension as a disaggregation.
-                        dimension_name = self.get_dimension_name(dimension)
-                        value_name = self.get_dimension_value_name(dimension, dimension_value)
-                        disaggregations[dimension_name] = value_name
+            # Get the indicator id.
+            indicator_id = self.get_series_id(series_key)
 
-                # Did we not find the indicator ID?
-                if indicator_id is None:
-                    continue
+            # Skip any series if we cannot figure out the indicator id.
+            if indicator_id is None:
+                continue
+            
+            # Get the indicator name if needed.
+            if indicator_id not in indicator_names:
+                indicator_names[indicator_id] = self.get_series_name(series_key)
+                # Also start off an empty list of rows.
+                indicator_data[indicator_id] = []
 
-                # Also use attributes as disaggregations.
-                for attribute_index, attribute_value_index in enumerate(attributes):
-                    if attribute_value_index is None:
-                        continue
-                    attribute = series_attributes[attribute_index]
-                    attribute_value = attribute['values'][attribute_value_index]
+            # Get the rows of data for this series.
+            indicator_data[indicator_id].extend(self.get_series_data(series_key))
 
-                    if attribute['id'] in self.drop_dimensions:
-                        # Ignore attributes specifically set to drop.
-                        continue
-                    elif self.drop_singleton_dimensions and len(attribute['values']) < 2:
-                        # Ignore attributes with only 1 variation.
-                        continue
-                    else:
-                        # Otherwise we will use this attribute as a disaggregation.
-                        attribute_name = self.get_dimension_name(attribute)
-                        value_name = self.get_dimension_value_name(attribute, attribute_value)
-                        disaggregations[attribute_name] = value_name
-
-                if indicator_id not in indicators:
-                    indicators[indicator_id] = []
-
-                row = self.get_row(year, value, disaggregations)
-                indicators[indicator_id].append(row)
-
-        # Convert the lists of dicts into dataframes.
-        for indicator_id in indicators:
-            data = pd.DataFrame(indicators[indicator_id])
-            # Enforce position of "Year" and "Value".
-            data = self.fix_dataframe_columns(data)
-            # Remove empty columns, because they are not necessary.
-            data = data.dropna(axis='columns', how='all')
-            # Create the Indicator object.
+        # Create the Indicator objects.
+        for indicator_id in indicator_data:
+            data = self.create_dataframe(indicator_data[indicator_id])
             name = indicator_names[indicator_id]
-            self.indicators[indicator_id] = sdg.Indicator(indicator_id, data=data, name=name)
+            indicator = sdg.Indicator(indicator_id, data=data, name=name)
+            self.indicators[indicator_id] = indicator

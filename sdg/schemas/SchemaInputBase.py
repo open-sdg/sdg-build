d682d3e8d6d6c8250c143413e28bed16f73fa930
Brock Fanning <brockfanning@gmail.com>
2019-11-16 12:32:00 -0500

    Print more stuff about errors

--- sdg/schemas/SchemaInputBase.py
+++ sdg/schemas/SchemaInputBase.py
@@ -61,11 +61,13 @@ class SchemaInputBase:
                 status = False
                 print('Validation errors for indicator ' + indicator.inid)
                 for error in self.validator.iter_errors(indicator.meta):
-                    print(error.message)
-                    print(error.schema)
-                    print(error.schema_path)
-                    print(error.validator)
-                    print(error.validator_value)
+                    ignore = ['properties', 'type']
+                    things = []
+                    for thing in error.schema_path:
+                        if thing not in ignore:
+                            things.append(thing)
+                    things = '/'.join(things)
+                    print('- ' + error.schema['title'] + ' (' + things + '): ' + error.message)
 
         return status
 

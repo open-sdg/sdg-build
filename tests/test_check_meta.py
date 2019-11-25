4b2f91d00b3ac29c652a3fe0410b0a07c85fde6c
Brock Fanning <brockfanning@gmail.com>
2019-11-25 13:18:03 -0500

    Syntax fix

--- tests/test_check_meta.py
+++ tests/test_check_meta.py
@@ -6,7 +6,7 @@ src_dir = os.path.dirname(os.path.realpath(__file__))
 
 def test_out_path():
     """Check that output_path is as expected"""
-    src_dir = os.path.realpath(src_dir)
+    real_src_dir = os.path.realpath(src_dir)
     schema_file = os.path.join('tests', '_prose.yml')
-    check_result = check_all_meta(src_dir=src_dir, schema_file=schema_file)
+    check_result = check_all_meta(src_dir=real_src_dir, schema_file=schema_file)
     assert check_result

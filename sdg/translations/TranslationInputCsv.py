63366b0c11cfb012d4cdbb53e4bb2dd6dcd56fea
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-01-17 15:03:00 +0000

    Fix indents

--- sdg/translations/TranslationInputCsv.py
+++ sdg/translations/TranslationInputCsv.py
@@ -33,24 +33,24 @@ class TranslationInputCsv(TranslationInputBase):
         """
         # Walk through the translation folder.
         for root, dirs, files in os.walk(folder):
-    # Each subfolder is a language code.
-    language = os.path.basename(root)
-    if language == folder:
-        continue
-    # Loop through the CSV files.
-    for file in files:
-        # Each CSV filename is a group.
-        file_parts = os.path.splitext(file)
-        group = file_parts[0]
-        extension = file_parts[1]
-        if extension != '.csv':
-            continue
-        csvdata = pd.read_csv(os.path.join(root, file), header=None)
-        # Loop through the CSV data to add the translations.
-        for index, row in csvdata.iterrows():
-            key=row[0]
-            value=row[1]
-            self.add_translation(language, group, key, value)
+            # Each subfolder is a language code.
+            language = os.path.basename(root)
+            if language == folder:
+                continue
+            # Loop through the CSV files.
+            for file in files:
+                # Each CSV filename is a group.
+                file_parts = os.path.splitext(file)
+                group = file_parts[0]
+                extension = file_parts[1]
+                if extension != '.csv':
+                    continue
+                csvdata = pd.read_csv(os.path.join(root, file), header=None)
+                # Loop through the CSV data to add the translations.
+                for index, row in csvdata.iterrows():
+                    key=row[0]
+                    value=row[1]
+                    self.add_translation(language, group, key, value)
 
     def execute(self):
         self.parse_csv(self.source)

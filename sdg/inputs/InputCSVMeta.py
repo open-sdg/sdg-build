086126f8ddec61bc9943a68deebc01df022947d1
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-02-10 17:41:21 +0000

    Update InputCSVMeta.py

--- sdg/inputs/InputCSVMeta.py
+++ sdg/inputs/InputCSVMeta.py
@@ -25,3 +25,27 @@ class InputCSVMeta(InputFiles):
             meta = sdg.meta.read_meta(inid, git=self.git, src_dir=src_dir)
             name = meta['indicator_name'] if 'indicator_name' in meta else None
             self.add_indicator(inid, name=name, meta=meta)
+           
+    def read_csv_meta(inid, git=True, src_dir='', git_data_dir=None):
+        fr = input_path(inid, ftype='meta', src_dir=src_dir)
+
+        metadata_mapping = pd.read_csv(os.path.join('metadata-mapping.csv'), header=None, names=["Field name", "Field value"])
+        meta_csv = pd.read_csv(fr, header=None, names=["Field name", "Field key"])
+
+        meta_df=pd.merge(meta_mapping, meta_csv, on="Field name")
+
+        meta=dict()
+        for row in meta_df.iterrows():
+            if type(row[1][2])==float:
+                if np.isnan(row[1][2])==False:
+                    meta[row[1][1]]=row[1][2]
+            else:
+                meta[row[1][1]]=row[1][2]
+
+        for i in range(1,7):
+            if "source_organisation_"+str(i) in meta:
+                meta["source_active_"+str(i)]="true"
+            else:
+                meta["source_active_"+str(i)]="false"
+
+        return meta

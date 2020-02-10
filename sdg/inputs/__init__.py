f691bd3a7c7714f7e71317d65e8f73d7a6fb54a2
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-02-10 18:11:48 +0000

    Update __init__.py

--- sdg/inputs/__init__.py
+++ sdg/inputs/__init__.py
@@ -2,7 +2,7 @@ from .InputBase import InputBase
 from .InputFiles import InputFiles
 from .InputCsvData import InputCsvData
 from .InputYamlMdMeta import InputYamlMdMeta
-from .InputYamlMdMeta import InputCSVMeta
+from .InputCSVMeta import InputCSVMeta
 from .InputSdmx import InputSdmx
 from .InputSdmxJson import InputSdmxJson
 from .InputSdmxMl_Structure import InputSdmxMl_Structure

a19da4574c6007a537c90929dc7d543b14fd5751
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-02-10 18:18:54 +0000

    Update InputCSVMeta.py

--- sdg/inputs/InputCSVMeta.py
+++ sdg/inputs/InputCSVMeta.py
@@ -20,13 +20,30 @@ class InputCSVMeta(InputFiles):
     def execute(self):
         """Get the metadata from the CSV, returning a list of indicators."""
         indicator_map = self.get_indicator_map()
+        metadata_mapping = pd.read_csv(os.path.join('metadata-mapping.csv'), header=None, names=["Field name", "Field value"])
         for inid in indicator_map:
             # Need to get the folder of the folder of the indicator file.
             src_dir = os.path.dirname(indicator_map[inid])
             src_dir = os.path.dirname(src_dir)                
-            meta = read_csv_meta(inid, git=self.git, src_dir=src_dir)
+            fr = input_path(inid, ftype='meta', src_dir=src_dir)
+            meta_csv = pd.read_csv(fr, header=None, names=["Field name", "Field key"])
+            meta_df=pd.merge(metadata_mapping, meta_csv, on="Field name")
+            meta=dict()
+            for row in meta_df.iterrows():
+                if type(row[1][2])==float:
+                    if np.isnan(row[1][2])==False:
+                        meta[row[1][1]]=row[1][2]
+                else:
+                    meta[row[1][1]]=row[1][2]
+            for i in range(1,7):
+                if "source_organisation_"+str(i) in meta:
+                    meta["source_active_"+str(i)]="true"
+                else:
+                    meta["source_active_"+str(i)]="false"
             name = meta['indicator_name'] if 'indicator_name' in meta else None
             self.add_indicator(inid, name=name, meta=meta)
+        
+        
            
     def read_csv_meta(inid, git=True, src_dir='', git_data_dir=None):
         fr = input_path(inid, ftype='meta', src_dir=src_dir)

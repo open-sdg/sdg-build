d627bb48b0b86b2f7bd61bd0e2ec013f749ab555
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-03-16 18:26:06 +0000

    Fix some errors

--- sdg/inputs/InputExcelMeta.py
+++ sdg/inputs/InputExcelMeta.py
@@ -15,13 +15,17 @@ class InputExcelMeta(InputFiles):
         git -- whether to use git information for dates in the metadata
         """
         self.git = git
+        self.metadata_mapping = metadata_mapping
+        self.sheet_number = sheet_number
         InputFiles.__init__(self, path_pattern)
 
     def execute(self):
         """Get the metadata from the CSV, returning a list of indicators."""
+        metadata_mapping=self.metadata_mapping
+        sheet_number=self.sheet_number
         indicator_map=self.get_indicator_map()
-        print(indicator_map)
-        meta_mapping = pd.read_csv(os.path.join(metadata-mapping), header=None, names=["Field name", "Field value"])
+        if metadata_mapping != None:
+            meta_mapping = pd.read_csv(os.path.join(metadata_mapping), header=None, names=["Field name", "Field value"])
         for inid in indicator_map:
             # Need to get the folder of the folder of the indicator file.
             src_dir = os.path.dirname(indicator_map[inid])
@@ -32,6 +36,7 @@ class InputExcelMeta(InputFiles):
             else:
                 fr = path
             meta_excel=pd.ExcelFile(fr)
+            meta_df=meta_excel.parse(meta_excel.sheet_names[sheet_number])
             for index, row in meta_df.iterrows():
                 if type(row[0])==float:
                     if np.isnan(row[0]):
@@ -39,7 +44,6 @@ class InputExcelMeta(InputFiles):
                         meta_df.iat[index, 1]=np.nan
             meta_df=meta_df.dropna()
             meta_df.columns=["Field name", "Field key"]
-            meta_df=meta_excel.parse(meta_excel.sheet_names[sheet_number])
             if metadata_mapping != None:         
                 meta_mapping_df=pd.merge(meta_mapping, meta_df, on="Field name")
                 meta=dict()

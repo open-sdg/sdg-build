5abf174f98170def1a1a9e8cc400b74fd56e769a
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-03-16 17:54:04 +0000

    Update InputExcelMeta.py

--- sdg/inputs/InputExcelMeta.py
+++ sdg/inputs/InputExcelMeta.py
@@ -19,9 +19,11 @@ class InputExcelMeta(InputFiles):
 
     def execute(self):
         """Get the metadata from the CSV, returning a list of indicators."""
+        metadata_mapping=self.metadata_mappung
         indicator_map=self.get_indicator_map()
         print(indicator_map)
-        meta_mapping = pd.read_csv(os.path.join(metadata-mapping), header=None, names=["Field name", "Field value"])
+        if metadata_mapping != None:
+            meta_mapping = pd.read_csv(os.path.join(metadata-mapping), header=None, names=["Field name", "Field value"])
         for inid in indicator_map:
             # Need to get the folder of the folder of the indicator file.
             src_dir = os.path.dirname(indicator_map[inid])
@@ -32,6 +34,7 @@ class InputExcelMeta(InputFiles):
             else:
                 fr = path
             meta_excel=pd.ExcelFile(fr)
+            meta_df=meta_excel.parse(meta_excel.sheet_names[sheet_number])
             for index, row in meta_df.iterrows():
                 if type(row[0])==float:
                     if np.isnan(row[0]):
@@ -39,7 +42,6 @@ class InputExcelMeta(InputFiles):
                         meta_df.iat[index, 1]=np.nan
             meta_df=meta_df.dropna()
             meta_df.columns=["Field name", "Field key"]
-            meta_df=meta_excel.parse(meta_excel.sheet_names[sheet_number])
             if metadata_mapping != None:         
                 meta_mapping_df=pd.merge(meta_mapping, meta_df, on="Field name")
                 meta=dict()

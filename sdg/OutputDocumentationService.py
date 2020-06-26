a78a25cd812424a789d79634fd666a5b6f9a4361
Brock Fanning <brockfanning@gmail.com>
2020-06-26 10:00:07 -0400

    CSV downloads and minor refactors

--- sdg/OutputDocumentationService.py
+++ sdg/OutputDocumentationService.py
@@ -93,38 +93,48 @@ class OutputDocumentationService:
             A list of dicts containing "title", "filename", and "content"
         """
         html = '<p>' + self.intro + '</p>'
-        html += '<div class="container">'
+        html += '<div>'
 
+        # Add all of the output pages.
         last_page = len(pages) - 1
         for num, page in enumerate(pages):
             if num % 3 == 0:
                 html += '<div class="row">'
-            title = '<h5 class="card-title">' + page['title'] + '</h5>'
-            description = '<p class="card-text">' + page['description'] + '</p>'
-            link = '<a href="' + page['filename'] + '" class="btn btn-primary">See examples</a>'
-            html += '<div class="col-sm"><div class="card"><div class="card-body">' + title + description + link + '</div></div></div>'
+            html += self.get_index_card_template().format(
+                title=page['title'],
+                description=page['description'],
+                destination=page['filename'],
+                call_to_action='See examples'
+            )
             if (num % 3 == 0 and num > 0) or num == last_page:
                 html += '</div>'
 
-        html += """
-                <div class="row">
-                    <div class="col-sm">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Disaggregation report</h5>
-                                <p class="card-text">These tables show information about all the disaggregations used in the data.</p>
-                                <a href="disaggregations" class="btn btn-primary">See report</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                """
-        html += '</div>'
+        # Add the disaggregation report (which is separate from the output pages).
+        html += '<div class="row my-5">' + self.get_index_card_template().format(
+            title='Disaggregation report',
+            description='These tables show information about all the disaggregations used in the data.',
+            destination='disaggregations',
+            call_to_action='See report'
+        ) + '</div>'
 
+        html += '</div>'
 
         page_html = self.get_html('Overview', html)
         self.write_page('index.html', page_html)
 
+
+    def get_index_card_template(self):
+        return """
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{title}</h5>
+                    <p class="card-text">{description}</p>
+                    <a href="{destination}" class="btn btn-primary">{call_to_action}</a>
+                </div>
+            </div>
+        </div>
+        """
 
     def write_disaggregation_report(self):
         os.makedirs(os.path.join(self.folder, 'disaggregations'), exist_ok=True)
@@ -157,19 +167,20 @@ class OutputDocumentationService:
             detail_filename = self.create_filename(disaggregation)
             self.write_disaggregation_detail_page(disaggregation, detail_filename, all_disaggregations[disaggregation])
             df_rows.append({
-                'Disaggregation': disaggregation,
+                'Disaggregation': '<a href="' + detail_filename + '">' + disaggregation + '</a>',
                 'Number of indicators': len(all_disaggregations[disaggregation]['indicators'].keys()),
                 'Number of values': len(all_disaggregations[disaggregation]['values'].keys()),
-                'Details': '<a href="' + detail_filename + '">Details</a>',
             })
         df = pd.DataFrame(df_rows)
         df.sort_values(by=['Disaggregation'], inplace=True)
-        report_html = self.get_html('Disaggregation report', df.to_html(escape=False, index=False, classes="table table-striped"))
+        table = df.to_html(escape=False, index=False, classes="table table-striped")
+        download_button = self.get_csv_download(df, 'disaggregations', 'disaggregation-report.csv')
+        report_html = self.get_html('Disaggregation report', download_button + table)
         report_path = os.path.join('disaggregations', 'index.html')
         self.write_page(report_path, report_html)
 
 
-    def write_disaggregation_detail_page(self, disaggregation, filepath, info):
+    def write_disaggregation_detail_page(self, disaggregation, filename, info):
         values_rows = []
         for value in info['values']:
             values_rows.append({
@@ -178,7 +189,9 @@ class OutputDocumentationService:
             })
         values_df = pd.DataFrame(values_rows)
         values_df.sort_values(by=['Value'], inplace=True)
-        values_html = '<h2>Values used in disaggregation</h2>' + values_df.to_html(index=False, classes="table table-striped")
+        values_header = '<h2>Values used in disaggregation</h2>'
+        values_download = self.get_csv_download(values_df, 'disaggregations', 'values--' + filename.replace('.html', '.csv'))
+        values_table = values_df.to_html(index=False, classes="table table-striped")
 
         indicators_rows = []
         for indicator_id in info['indicators']:
@@ -187,10 +200,49 @@ class OutputDocumentationService:
             })
         indicators_df = pd.DataFrame(indicators_rows)
         indicators_df.sort_values(by=['Indicator'], inplace=True)
-        indicators_html = '<h2>Indicators using disaggregation</h2>' + indicators_df.to_html(index=False, classes="table table-striped")
+        indicators_header = '<h2>Indicators using disaggregation</h2>'
+        indicators_download = self.get_csv_download(indicators_df, 'disaggregations', 'indicators--' + filename.replace('.html', '.csv'))
+        indicators_table = indicators_df.to_html(index=False, classes="table table-striped")
 
-        detail_html = self.get_html('Disaggregation: ' + disaggregation, values_html + indicators_html)
-        self.write_page(os.path.join('disaggregations', filepath), detail_html)
+        detail_html = self.get_html('Disaggregation: ' + disaggregation, self.get_disaggregation_detail_template().format(
+            values_header=values_header,
+            values_download=values_download,
+            values_table=values_table,
+            indicators_header=indicators_header,
+            indicators_download=indicators_download,
+            indicators_table=indicators_table
+        ))
+        self.write_page(os.path.join('disaggregations', filename), detail_html)
+
+
+    def get_disaggregation_detail_template(self):
+        return """
+        <div>
+            {values_header}
+            {values_download}
+            {values_table}
+        </div>
+        <div>
+            {indicators_header}
+            {indicators_download}
+            {indicators_table}
+        </div>
+        """
+
+
+    def get_csv_download(self, df, path, filename):
+        csv_path = os.path.join(self.folder, path, filename)
+        df = df.replace('<[^<]+?>', '', regex=True)
+        df.to_csv(csv_path, index=False)
+        return self.get_download_button_template().format(filename=filename)
+
+
+    def get_download_button_template(self):
+        return """
+        <div class="my-3">
+            <a href="{filename}" class="btn btn-primary">Download CSV</a>
+        </div>
+        """
 
 
     def get_html(self, title, content):

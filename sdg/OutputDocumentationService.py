e3314d20d70a357cd9f4af03adb94fba7114f2dd
Brock Fanning <brockfanning@gmail.com>
2020-06-27 09:56:38 -0400

    Fix

--- sdg/OutputDocumentationService.py
+++ sdg/OutputDocumentationService.py
@@ -317,11 +317,7 @@ class OutputDocumentationService:
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js" integrity="sha256-dtGH1XcAyKopMui5x20KnPxuGuSx9Rs6piJB/4Oqu6I=" crossorigin="anonymous"></script>
-            <script>
-                $(function() {
-                    $(".tablesorter").tablesorter();
-                });
-            </script>
+            <script>$(".tablesorter").tablesorter();</script>
         </html>
         """
         return template.format(branding=self.branding, title=title, content=content)

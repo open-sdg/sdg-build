37223a24f587d2e8f1976046d232793e721297c1
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-08-03 12:17:37 +0100

    Update InputCsvData.py

--- sdg/inputs/InputCsvData.py
+++ sdg/inputs/InputCsvData.py
@@ -14,6 +14,8 @@ class InputCsvData(InputFiles):
     def execute(self, indicator_options):
         """Get the data, edges, and headline from CSV, returning a list of indicators."""
         indicator_map = self.get_indicator_map()
+        print("!!! indicator_map (CSV) !!!")
+        print(indicator_map)
         for inid in indicator_map:
             data = pd.read_csv(indicator_map[inid])
             self.add_indicator(inid, data=data, options=indicator_options)

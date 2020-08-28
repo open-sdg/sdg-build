bee0db23ccf0953dfea406ef27e9cdfdb839a6c2
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-08-28 10:55:58 +0100

    Update __init__.py

--- sdg/__init__.py
+++ sdg/__init__.py
@@ -18,6 +18,7 @@ from . import schemas
 from . import translations
 from .OutputDocumentationService import OutputDocumentationService
 from .Indicator import Indicator
+from .IndicatorDownloadService import IndicatorDownloadService
 from .IndicatorExportService import IndicatorExportService
 from .IndicatorOptions import IndicatorOptions
 from .Series import Series

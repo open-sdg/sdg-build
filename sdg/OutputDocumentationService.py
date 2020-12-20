894438b2797665ca4b866e3d3bda44792330d885
Brock Fanning <brockfanning@gmail.com>
2020-12-20 18:51:25 -0500

    Show page title before branding

--- sdg/OutputDocumentationService.py
+++ sdg/OutputDocumentationService.py
@@ -319,7 +319,7 @@ class OutputDocumentationService:
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <title>{branding} - {title}</title>
+            <title>{title} - {branding}</title>
 
             <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">

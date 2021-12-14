a5dd49a0b4ce123f8b9166dd6630ed008bae4b62
Brock Fanning <brockfanning@gmail.com>
2021-12-14 10:29:32 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='sdg',
-      version='1.6.0',
+      version='1.6.1',
       description='Build SDG data and metadata into output formats',
       url='https://github.com/open-sdg/sdg-build',
       author='Doug Ashton',

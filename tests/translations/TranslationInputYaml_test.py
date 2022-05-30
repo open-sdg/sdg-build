8b66822dbc4aad71407dda1de622060bf5095ff2
Brock Fanning <brockfanning@gmail.com>
2022-05-30 13:07:33 -0400

    Test coverage for TranslationInputYaml

--- tests/translations/TranslationInputYaml_test.py
+++ tests/translations/TranslationInputYaml_test.py
@@ -3,8 +3,8 @@ import os
 
 def test_translation_input_csv():
 
-    input = sdg.translations.TranslationInputCsv(
-        source=os.path.join('tests', 'assets', 'translations', 'csv'),
+    input = sdg.translations.TranslationInputYaml(
+        source=os.path.join('tests', 'assets', 'translations', 'yaml'),
     )
     input.execute()
     assert input.translations['en']['foo']['foo'] == 'bar'

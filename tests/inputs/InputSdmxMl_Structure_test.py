6987e14c576d13e210830e8dc0cd5a6316269bde
brockfanning <brockfanning@gmail.com>
2022-05-30 07:51:45 -0400

    Testing revamp (#306)
    Codecov and new tests for inputs and outputs

---
+++ tests/inputs/InputSdmxMl_Structure_test.py
@@ -0,0 +1,15 @@
+import sdg
+import os
+import inputs_common
+
+def test_sdmx_structure_specific_input():
+
+    data_path = os.path.join('tests', 'data', 'sdmx', 'structure', '1-1-1--structure.xml')
+    data_input = sdg.inputs.InputSdmxMl_Structure(
+        source=data_path,
+        import_codes=True,
+    )
+    indicator_options = sdg.IndicatorOptions()
+    data_input.execute(indicator_options=indicator_options)
+
+    inputs_common.assert_input_has_correct_data(data_input.indicators['1-1-1'].data)

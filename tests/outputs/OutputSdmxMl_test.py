0454f908b5cfeed61c1677858f7b7503a1826042
Brock Fanning <brockfanning@gmail.com>
2022-05-29 20:10:14 -0400

    Progress

--- tests/outputs/OutputSdmxMl_test.py
+++ tests/outputs/OutputSdmxMl_test.py
@@ -10,10 +10,15 @@ def test_sdmx_ml_output():
         import_codes=True,
         drop_singleton_dimensions=False,
     )
-    schema_path = os.path.join('tests', '_prose.yml')
+    schema_path = os.path.join('tests', 'meta2', 'metadata_schema.yml')
     schema = sdg.schemas.SchemaInputOpenSdg(schema_path=schema_path)
-    data_output = sdg.outputs.OutputSdmxMl([data_input], schema, structure_specific=True)
-    data_output.execute()
+    data_output = sdg.outputs.OutputSdmxMl([data_input], schema,
+        structure_specific=True,
+        meta_reporting_type='N',
+        meta_ref_area=862,
+    )
+    assert data_output.validate()
+    assert data_output.execute()
 
     output_path = os.path.join('_site', 'sdmx', '1-1-1.xml')
     output_input = sdg.inputs.InputSdmxMl_StructureSpecific(

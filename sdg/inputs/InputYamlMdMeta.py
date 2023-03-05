65a4fa125bb5b9cb498a54dc88c74376f6a4dd6c
Brock Fanning <brockfanning@gmail.com>
2023-03-05 13:22:02 -0500

    Better exception messages for InputYamlMdMeta

--- sdg/inputs/InputYamlMdMeta.py
+++ sdg/inputs/InputYamlMdMeta.py
@@ -1,11 +1,23 @@
 from sdg.inputs import InputMetaFiles
+from ruamel.yaml.parser import ParserError
 import yamlmd
+import sys
 
 class InputYamlMdMeta(InputMetaFiles):
     """Sources of SDG metadata that are local .md files containing Yaml."""
 
     def read_meta_at_path(self, filepath):
-        meta_md = yamlmd.read_yamlmd(filepath)
-        meta = dict(meta_md[0])
-        meta['page_content'] = ''.join(meta_md[1])
+        meta = {}
+        try:
+            meta_md = yamlmd.read_yamlmd(filepath)
+            meta = dict(meta_md[0])
+            meta['page_content'] = ''.join(meta_md[1])
+        except ParserError as e:
+            print('The file at ' + filepath + ' could not be parsed because of a syntax error.')
+            print('Syntax errors often involve single/double quotes and/or colons (:).')
+            print('Sometimes you can find the problem by looking at the lines/columns mentioned in the following raw error message:')
+            print('------')
+            print(e)
+            print('------')
+            sys.exit(1)
         return meta

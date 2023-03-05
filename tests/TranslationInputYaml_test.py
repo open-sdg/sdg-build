11776687d272063582b5e89b17df8e9f94cd8123
Brock Fanning <brockfanning@gmail.com>
2023-03-05 15:26:00 -0500

    Test translations

--- tests/TranslationInputYaml_test.py
+++ tests/TranslationInputYaml_test.py
@@ -1,10 +1,19 @@
 import sdg
 import os
+import pytest
 
-def test_translation_input_csv():
+def test_translation_input_yaml():
 
     input = sdg.translations.TranslationInputYaml(
         source=os.path.join('tests', 'assets', 'translations', 'yaml'),
     )
     input.execute()
     assert input.translations['en']['foo']['foo'] == 'bar'
+
+def test_translation_input_yaml_with_problems():
+
+    input = sdg.translations.TranslationInputYaml(
+        source=os.path.join('tests', 'assets', 'translations', 'yaml-with-problems'),
+    )
+    with pytest.raises(Exception) as e_info:
+        input.execute()
